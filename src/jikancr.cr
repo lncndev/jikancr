99e25acbd7e59af4683cb05ea4e29706b85995f1
Onodera Project <lincoln@chuuo.net>
2021-09-25 11:14:41 -0400

    Update jikancr.cr

--- src/jikancr.cr
+++ src/jikancr.cr
@@ -3,12 +3,12 @@ require "json"
 
 module Jikan
 
-  def self.manga(id : String | Int64)
+  def self.manga(id : String || Int64)
     response = HTTP::Client.get("https://api.jikan.moe/v3/manga/" + id)
     return JSON.parse(response.body)
   end
 
-  def self.anime(id : String | Int64)
+  def self.anime(id : String || Int64)
     response = HTTP::Client.get("https://api.jikan.moe/v3/anime/" + id)
     return JSON.parse(response.body)
   end
