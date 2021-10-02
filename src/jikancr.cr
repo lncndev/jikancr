774925d4b7338a44af4d12ddb7d31058275dabbf
lncndev <lincoln@chuuo.net>
2021-10-02 17:57:42 -0400

    Some fixes

--- src/jikancr.cr
+++ src/jikancr.cr
@@ -3,17 +3,17 @@ require "json"
 
 module Jikan
 
-  def self.manga(id : String || Int64)
+  def self.manga(id)
     response = HTTP::Client.get("https://api.jikan.moe/v3/manga/" + id)
     return JSON.parse(response.body)
   end
 
-  def self.anime(id : String || Int64)
+  def self.anime(id)
     response = HTTP::Client.get("https://api.jikan.moe/v3/anime/" + id)
     return JSON.parse(response.body)
   end
 
-  def self.search(query : String)
+  def self.search(query)
     response = HTTP::Client.get("https://api.jikan.moe/v3/search?q=" + query.as(String))
     return JSON.parse(response.body)
   end
