91df69d2ce2f726bd4f7db4eecc89121610fca03
lncndev <lincoln@chuuo.net>
2021-10-03 18:27:48 -0400

    Fixed search

--- src/jikancr.cr
+++ src/jikancr.cr
@@ -4,17 +4,17 @@ require "json"
 module Jikan
 
   def self.manga(id)
-    response = HTTP::Client.get("https://api.jikan.moe/v3/manga/" + id)
+    response = HTTP::Client.get("https://api.jikan.moe/v3/manga/" + id.to_s)
     return JSON.parse(response.body)
   end
 
   def self.anime(id)
-    response = HTTP::Client.get("https://api.jikan.moe/v3/anime/" + id)
+    response = HTTP::Client.get("https://api.jikan.moe/v3/anime/" + id.to_s)
     return JSON.parse(response.body)
   end
 
-  def self.search(query)
-    response = HTTP::Client.get("https://api.jikan.moe/v3/search?q=" + query.as(String))
+  def self.search(type, query)
+    response = HTTP::Client.get("https://api.jikan.moe/v3/search/" + type.to_s + "?q=" + query.to_s + "&limit=1")
     return JSON.parse(response.body)
   end
 
